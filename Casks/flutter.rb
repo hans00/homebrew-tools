07ff36d7a926a5c7063e4f45cdf5abad8a22d91d
Hans <hans00@users.noreply.github.com>
2019-07-10 19:10:02 +0800

    update flutter version

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -3,7 +3,7 @@ cask 'flutter' do
   name "Flutter"
   homepage "https://www.flutter.dev"
 
-  version "1.2.1"
+  version "1.7.8+hotfix.3"
   sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
 
